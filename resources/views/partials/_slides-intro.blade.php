f19aa4f71e6b9d6da98c013392be6e2d41aae429
cristian <cristianfaune.t@gmail.com>
2017-01-15 09:48:42 -0300

    arreglo menú en xs devices

--- resources/views/partials/_slides-intro.blade.php
+++ resources/views/partials/_slides-intro.blade.php
@@ -8,7 +8,7 @@
 
 </div><!--slides-->
 
-<div class="intro-text container">
+<div class="intro-text container hidden-xs">
 	<h1>Tu mascota en las mejores manos</h1>
 	<p>Todos los días y las 24 horas al servicio de tu pequeño amigo.</p>
 	<!--<span class="btn-round btn-blue">Call Us: (000) 111-2222</span>-->
